963b95a5b2f89d2616e63cc1532f96d001c3be97
Jeff Allen <1063272+jeffallen6767@users.noreply.github.com>
2017-08-13 13:02:46 -0700

    keccak needs to be generated

--- postinstall.js
+++ postinstall.js
@@ -1,21 +1,17 @@
+// transform keccak to Daytrader.plugin:
 var fs = require('fs'),
-  
-  what;
-
-// pkg keccak for web:
-var file = fs.readFileSync("./node_modules/keccak-p-js/src/keccak.js", 'utf8');
-//console.log("file", file);
-
-var wrapped = [
-"/*!",
- "* Daytrader v1.0.0",
- "* https://github.com/jeffallen6767/daytrader",
- "*",
- "* Copyright 2017 Jeffrey David Allen",
- "*/",
-"Daytrader.plugin('keccak', function(app) {",
-file.replace("module.exports = keccak;", "return keccak;"),
-"});"
-].join("\n");
+  file = fs.readFileSync("./node_modules/keccak-p-js/src/keccak.js", 'utf8'),
+  wrapped = [
+    "/*!",
+     "* Daytrader v1.0.0",
+     "* https://github.com/jeffallen6767/daytrader",
+     "*",
+     "* Copyright 2017 Jeffrey David Allen",
+     "*/",
+    "Daytrader.plugin('keccak', function(app) {",
+    file.replace("module.exports = keccak;", "return keccak;"),
+    "});",
+    ""
+  ].join("\n");
 
 fs.writeFileSync("./public/js/keccak.js", wrapped);
