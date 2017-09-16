88964dd614c48394ca8141ecbb4bfbd441fda43f
Jeff Allen <1063272+jeffallen6767@users.noreply.github.com>
2017-09-16 03:18:58 -0700

    individual stock stats generation works

--- public/js/pages/symbol.js
+++ public/js/pages/symbol.js
@@ -29,23 +29,118 @@ Daytrader.plugin("symbol", function(app) {
         stats = app.stats.get(recs, symbol),
         total = stats.total,
         shares = stats.shares,
+        transactions = stats.transactions || [],
+        dividends = stats.dividends || [],
         positiveTotal = total >= 0,
         positiveShares = shares >= 0,
         totalLabel = positiveTotal ? "Gain" : "Loss",
         totalCss = positiveTotal ? "blackBold" : "redBold",
         sharesCss = positiveShares ? "blackBold" : "redBold",
-        statsGui = $('<div class="symbol-stats-container">').append([
-          $('<span class="' + sharesCss + '">Current Shares: ' + shares + '</span>'),
-          $('<span class="horizontal-spacer">&nbsp;</span>'),
+        rows = [],
+        statsGui = $('<div class="symbol-stats-container">'),
+        temp;
+      
+      // shares
+      if (shares > 0) {
+        rows.push(
+          $([
+            '<span class="',
+            sharesCss,
+            '">Current Shares: ',
+            shares,
+            ' @ ',
+            math.money(stats.each),
+            ' = ',
+            math.money(stats.cost),
+            '</span>'
+          ].join(app.CONST_NO_SPACE))
+        );
+        rows.push(
+          $('<div></div>')
+        );
+      }
+      
+      // transactions
+      if (transactions.length) {
+        rows.push(
+            $('<div>History:</div>')
+        );
+        transactions.forEach(function(trans, index) {
+          var 
+            idx = index + 1,
+            num = trans.num,
+            buy = trans.buy,
+            sell = trans.sell,
+            diff = trans.diff,
+            css = diff >= 0 ? "transaction blackBold" : "transaction redBold";
+          rows.push(
+            $([
+              '<span class="',
+              css,
+              '">',
+              idx,
+              ': ',
+              num,
+              ' buy @ ',
+              math.money(buy / num),
+              ' sell @ ',
+              math.money(sell / num),
+              ' = ',
+              math.money(diff),
+              '</span>'
+             ].join(app.CONST_NO_SPACE))
+          );
+          rows.push(
+            $('<div></div>')
+          );
+        });
+        temp = stats["trans-stats"];
+        rows.push(
+          $([
+            '<span class="blackBold">Transaction Gains:',
+            math.money(temp.gain),
+            ', <span class="redBold">Losses:',
+            math.money(temp.loss),
+            '</span>, Total:',
+            math.money(temp.total),
+            '</span>'
+           ].join(app.CONST_NO_SPACE))
+        );
+        rows.push(
+          $('<div></div>')
+        );
+      }
+      
+      // dividends
+      if (dividends.length) {
+        temp = dividends.reduce(function(total, dividend) {
+          total += dividend.amount;
+          return total;
+        }, 0);
+        rows.push(
+          $([
+            '<span class="blackBold">Dividend Gains:',
+            math.money(temp),
+            '</span>'
+           ].join(app.CONST_NO_SPACE))
+        );
+        rows.push(
+          $('<div></div>')
+        );
+      }
+      
+      if (total) {
+        rows.push(
           $('<span class="' + totalCss + '">Total ' + totalLabel + ': ' + math.money(total) + '</span>')
-        ]);
+        );
+      }
       
       console.log("symbol", symbol);
       console.log("recs", recs);
       
       titleEl.text(symbolTitle);
       
-      app.display.dom.replace(statsEl, statsGui);
+      app.display.dom.replace(statsEl, statsGui.append(rows));
 
       app.display.show.table("All", recs, contentEl);
     };
--- public/js/stats.js
+++ public/js/stats.js
@@ -26,25 +26,169 @@ Daytrader.plugin("stats", function(app) {
         sorted.forEach(function(rec, idx) {
           console.log(idx, rec);
           var key = rec.symbol,
-            dat;
+            action = rec.action,
+            amt = Math.abs(rec.amount), 
+            qty = Math.abs(rec.quantity),
+            fee = Math.abs(rec.commission) + Math.abs(rec.fees),
+            dat, 
+            temp,
+            trans,
+            item,
+            cost,
+            num,
+            cash,
+            combo;
           if (!data[key]) {
             data[key] = {
+              "in": 0,
+              "out": 0,
+              "fee": 0,
               "total": 0,
-              "shares": 0
+              "shares": 0,
+              "cost": 0,
+              "acc": [],
+              "transactions": [],
+              "trans-stats": {},
+              "dividends": []
             };
           }
           dat = data[key];
-          dat.total = math.add(dat.total, rec.amount);
-          switch (rec.action) {
+          switch (action) {
             case "Buy":
+              dat.out = math.sub(dat.out, amt);
+              dat.fee = math.sub(dat.fee, fee);
+              dat.total = math.sub(dat.total, amt, fee);
+              dat.shares = math.add(dat.shares, qty);
+              cash = amt + fee;
+              dat.acc.push({
+                "more": symbol,
+                "num": qty,
+                "cost": cash,
+                "each": cash / qty
+              });
+              combo = dat.acc.reduce(function(acc, obj) {
+                var 
+                  newNum = acc.num + obj.num,
+                  newCost = acc.cost + obj.cost,
+                  newEach = newCost / newNum,
+                  result = {
+                    "more": symbol,
+                    "num": newNum,
+                    "cost": newCost,
+                    "each": newEach
+                  };
+                return result;
+              });
+              console.log("+buy=acc.combo", JSON.stringify(combo));
+              dat.acc = [combo];
+              break;
             case "Sell":
+              dat.in = math.add(dat.in, amt);
+              dat.fee = math.sub(dat.fee, fee);
+              dat.total = math.add(dat.total, amt, -1 * fee);
+              dat.shares = math.sub(dat.shares, qty);
+              cash = amt - fee;
+              temp = qty;
+              trans = {
+                "num": 0,
+                "buy": 0,
+                "sell": cash
+              };
+              console.log("start sale...", temp, JSON.stringify(dat.acc));
+              while (temp > 0 && dat.acc.length) {
+                item = dat.acc[0];
+                num = item.num;
+                cost = item.cost;
+                console.log("selling...", temp, num, cost, " @ ", item.each);
+                if (num <= temp) {
+                  // equal or less
+                  temp -= num;
+                  cash -= cost;
+                  dat.acc.shift();
+                  trans.num += num;
+                  trans.buy += cost;
+                } else {
+                  // more
+                  item.num -= temp;
+                  item.cost = item.num * item.each;
+                  trans.num += temp;
+                  trans.buy += temp * item.each;
+                  temp = 0;
+                }
+              }
+              trans.diff = trans.sell - trans.buy;
+              console.log("sold...", JSON.stringify(trans));
+              console.log("dat.acc...", JSON.stringify(dat.acc));
+              dat.transactions.push(trans);
+              break;
+            case "Dividend":
+              dat.total = math.add(dat.total, amt);
+              dat.dividends.push(rec);
+              break;
             case "Stock Dividend":
-              dat.shares = math.add(dat.shares, rec.quantity);
+              dat.shares = math.add(dat.shares, qty);
+              dat.acc.push({
+                "more": symbol,
+                "num": qty,
+                "cost": 0,
+                "each": 0
+              });
+              combo = dat.acc.reduce(function(acc, obj) {
+                var 
+                  newNum = acc.num + obj.num,
+                  newCost = acc.cost + obj.cost,
+                  newEach = newCost / newNum,
+                  result = {
+                    "more": symbol,
+                    "num": newNum,
+                    "cost": newCost,
+                    "each": newEach
+                  };
+                return result;
+              });
+              console.log("+stock_div=acc.combo", JSON.stringify(combo));
+              dat.acc = [combo];
+              break;
+            case "IRA Receipt":
+            case "Credit Interest":
+            case "Rebate":
+            case "Cash Adjustment":
+              // TODO?
+              break;
+            default:
+              throw new app.Err("stats", ["Unknown rec.action[", idx, "]: ", action].join(app.CONST_NO_SPACE), rec);
               break;
           }
         });
         console.log("data", data);
         result = upperSymbol ? data[upperSymbol] : data;
+        if (result.acc && result.acc.length) {
+          result.cost = result.acc.reduce(function(num, item) {
+            num += item.cost;
+            return num;
+          }, 0);
+          result.each = result.cost / result.shares;
+          result.total += result.cost;
+        }
+        // "trans-stats"
+        if (result.transactions && result.transactions.length) {
+          temp = result["trans-stats"] = {
+            "gain": 0,
+            "loss": 0,
+            "total": 0
+          };
+          result.transactions.forEach(function(transaction) {
+            var diff = transaction.diff;
+            if (diff > 0) {
+              temp.gain += diff;
+            } else {
+              temp.loss += diff;
+            }
+            temp.total += diff;
+          });
+        }
+        
+        
         console.log("result", result);
         return result;
       }
